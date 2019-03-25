791af96826adc15af3da360a3b8d9c88e58c13b0
Anuraag Yachamaneni <ayachama@purdue.edu>
2019-03-25 19:48:46 -0400

    Add inital code and template

---
+++ dist/js/scripts.js
@@ -0,0 +1,48 @@
+companies = [
+  { logo: "amerlight", url: "https://www.amerlight.com" },
+  { logo: "asimpleart", url: "https://asimpleart.com" },
+  { logo: "bloomerang", url: "https://bloomerang.co/" },
+  { logo: "bolstra", url: "https://bolstra.com/" },
+  { logo: "canceriq", url: "https://www.canceriq.com" },
+  { logo: "cooler", url: "https://www.coolerscreens.com" },
+  { logo: "crafty", url: "https://craftydelivers.com" },
+  { logo: "crema", url: "https://crema.us" },
+  { logo: "doublemap", url: "https://www.doublemap.com" },
+  { logo: "fillintime", url: "https://fillintimellc.wixsite.com/fill-in-time" },
+  { logo: "freeosk", url: "https://www.thefreeosk.com/" }
+]
+
+judges = [
+  { name: "Judge 1", image: "", company:"", role: "", linkedin: "https://www.amerlight.com" }
+]
+
+
+
+const companiesContainer = document.getElementById("companies-attending");
+
+companies.forEach(company => {
+  let companyLink = document.createElement("a");
+  let companyLogo = document.createElement("img");
+
+  companyLink.href = company.url; 
+  companyLogo.src = `img/${company.logo}.png`;
+  companyLogo.className = "logo-image";
+
+  companyLink.append(companyLogo);
+  companiesContainer.append(companyLink);
+})
+
+
+const judgesContainer = document.getElementById("judges-attending");
+
+judges.forEach(judge => {
+  let judgeLink = document.createElement("a");
+  let judgeLogo = document.createElement("img");
+
+  judgeLink.href = judge.linkedin; 
+  judgeLogo.src = `img/${judge.image}.png`;
+  judgeLogo.className = "judge-image";
+
+  judgeLink.append(judgeLogo);
+  judgesContainer.append(judgeLink);
+})
